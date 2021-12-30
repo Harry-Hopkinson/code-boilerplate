26fe6a08c560e9cabdb10ba8f94be71842a194df
Harry-Hopkinson <harryjhopkinson@gmail.com>
2021-12-30 18:07:20 +0000

    Finds the file path and gives error if otherwise

--- src/extension.ts
+++ src/extension.ts
@@ -1,24 +1,33 @@
 import * as vscode from 'vscode';
+import "typescript";
 
 export function activate(context: vscode.ExtensionContext) {
 	console.log("code-boilerplate is active!");
 	
 	let disposable = vscode.commands.registerCommand('code-boilerplate.CodeBoilerPlate', () => {
 	
-		fileType =
-		fileName = vscode.window.activeTextEditor?.document.fileName;
+		
+		const activeEditor = vscode.window.activeTextEditor;
+		if (activeEditor) {
+			const documentFileType = activeEditor.document.uri.path;
+		}
+		else {
+			vscode.window.showInformationMessage("You need to have a valid document...");
+			break;
+		}
+		const documentFileName = vscode.window.activeTextEditor?.document.fileName;
 
-		if fileType === 'ts' {
+		if documentFileType === 'ts' {
 			vscode.window.activeTextEditor.edit(editBuilder => {
 				editBuilder.insert(new vscode.Position(0, 0), 'console.log("Hello World!");');
 			});
 		}
-		else if fileType === "py" {
+		else if documentFileType === "py" {
 			vscode.window.activeTextEditor.edit(editBuilder => {
 				editBuilder.insert(new vscode.Position(0, 0), 'print("Hello World!")');
 			});
 		}
-		else if fileType === "js" {
+		else if documentFileType === "js" {
 			vscode.window.activeTextEditor?.edit(editBuilder => {
 				editBuilder.insert(new vscode.Position(0,0), 'console.log("Hello World!")');
 			})
