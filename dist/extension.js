d9eeaaa9c534966797bff2b6def01e8c73779905
Harry-Hopkinson <harryjhopkinson@gmail.com>
2021-12-30 16:39:44 +0000

    Cleaned up code and deleted tests

--- dist/extension.js
+++ dist/extension.js
@@ -50,20 +50,31 @@ const vscode = __webpack_require__(1);
 function activate(context) {
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "code-boilerplate" is now active!');
+    console.log("code-boilerplate is active!");
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('code-boilerplate.helloWorld', () => {
-        // The code you place here will be executed every time your command is executed
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World from code-boilerplate!');
+    let disposable = vscode.commands.registerCommand('code-boilerplate.CodeBoilerPlate', () => {
+        // pop up asking for extension file type
+        if (fileType === '.ts') {
+            vscode.window.activeTextEditor.edit(editBuilder => {
+                editBuilder.insert(new vscode.Position(0, 0), 'console.log("Hello World!");');
+            });
+        }
+        else if (fileType === "python") {
+            vscode.window.activeTextEditor.edit(editBuilder => {
+                editBuilder.insert(new vscode.Position(0, 0), 'print("Hello World!")');
+            });
+        }
+        vscode.window.showInformationMessage("Generating code boilerplate...");
     });
     context.subscriptions.push(disposable);
 }
 exports.activate = activate;
 // this method is called when your extension is deactivated
-function deactivate() { }
+function deactivate() {
+    console.log("code-boilerplate is deactivated!");
+}
 exports.deactivate = deactivate;
 
 })();
--- src/extension.ts
+++ src/extension.ts
@@ -1,28 +1,19 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log("code-boilerplate is active!");
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
+	
 	let disposable = vscode.commands.registerCommand('code-boilerplate.CodeBoilerPlate', () => {
 	
-		// pop up asking for extension file type
+		// pop up asking for extension file type - user input
+		fileType = 
 
-		if fileType === '.ts' {
+		if fileType === 'ts' {
 			vscode.window.activeTextEditor.edit(editBuilder => {
 				editBuilder.insert(new vscode.Position(0, 0), 'console.log("Hello World!");');
 			});
 		}
-		else if fileType === "python" {
+		else if fileType === "py" {
 			vscode.window.activeTextEditor.edit(editBuilder => {
 				editBuilder.insert(new vscode.Position(0, 0), 'print("Hello World!")');
 			});
