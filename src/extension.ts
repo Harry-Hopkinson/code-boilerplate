5159b7ddda0ab41a66cb68a2e7888df79018d753
Harry-Hopkinson <harryjhopkinson@gmail.com>
2021-12-30 21:13:04 +0000

    File type now works

--- src/extension.ts
+++ src/extension.ts
@@ -6,34 +6,14 @@ export function activate(context: vscode.ExtensionContext) {
 	
 	let disposable = vscode.commands.registerCommand('code-boilerplate.CodeBoilerPlate', () => {
 	
-		
-		const activeEditor = vscode.window.activeTextEditor;
-		if (activeEditor) {
-			const documentFileType = activeEditor.document.uri.path;
-		}
-		else {
-			vscode.window.showInformationMessage("You need to have a valid document...");
-			break;
-		}
+		const documentFileType = vscode.window.activeTextEditor?.document.languageId;
 		const documentFileName = vscode.window.activeTextEditor?.document.fileName;
 
-		if documentFileType === 'ts' {
-			vscode.window.activeTextEditor.edit(editBuilder => {
-				editBuilder.insert(new vscode.Position(0, 0), 'console.log("Hello World!");');
-			});
-		}
-		else if documentFileType === "py" {
-			vscode.window.activeTextEditor.edit(editBuilder => {
-				editBuilder.insert(new vscode.Position(0, 0), 'print("Hello World!")');
-			});
-		}
-		else if documentFileType === "js" {
-			vscode.window.activeTextEditor?.edit(editBuilder => {
-				editBuilder.insert(new vscode.Position(0,0), 'console.log("Hello World!")');
-			})
+		if (documentFileType === "javascript") {
+			
 		}
 		
-			vscode.window.showInformationMessage("Generating code boilerplate...");
+		vscode.window.showInformationMessage("Generating code boilerplate...");
 	});
 
 	context.subscriptions.push(disposable);
