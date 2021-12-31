0971867edec3fe65b27f983f9452863e97f98dcb
Harry-Hopkinson <harryjhopkinson@gmail.com>
2021-12-31 18:44:56 +0000

    Status Bar Fixed

--- src/StatusBar.ts
+++ src/StatusBar.ts
@@ -1,9 +1,39 @@
-import * as vscode from "vscode";
-import "./extension";
+import { StatusBarItem, window, StatusBarAlignment } from 'vscode';
 
-let statusBar: vscode.StatusBarItem;
+export class StatusbarUi {
 
-export function statusBarFunc({ subscriptions }: vscode.ExtensionContext) {
-    statusBar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
-    subscriptions.push(statusBar);
+    private static _statusBarItem: StatusBarItem;
+
+    private static get statusbar() {
+        if (!StatusbarUi._statusBarItem) {
+            StatusbarUi._statusBarItem = window
+                .createStatusBarItem(StatusBarAlignment.Right, 100);
+        }
+
+        return StatusbarUi._statusBarItem;
+    }
+
+    static Init() {
+        StatusbarUi.Working('Loading...');
+        setTimeout(function () {
+            StatusbarUi.Live();
+        }, 1000);
+    }
+
+    static Working(workingMsg: string = 'Working on it...') {
+        StatusbarUi.statusbar.text = `$(pulse) ${workingMsg}`;
+    }
+
+    public static Live() {
+        StatusbarUi.statusbar.text = '$(broadcast) Hello World';
+        StatusbarUi.statusbar.tooltip = 'Click to generate Code';
+    }
+
+    public static Offline(port: Number) {
+        StatusbarUi.statusbar.text = `Code generated`;
+    }
+
+    public static dispose() {
+        StatusbarUi.statusbar.dispose();
+    }
 }
